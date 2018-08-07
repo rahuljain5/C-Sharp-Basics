cf6c748b7e2fe2e51a04c07037fab299875370a2
Rahul Jain <rjain.rahul5@gmail.com>
2018-08-07 18:04:47 +0530

    Add files via upload

---
+++ PrimeSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_Programs
+{
+    class PrimeSum
+    {
+        static void Main()
+        {
+            int n = 2, m = 20, sum =0;
+            //Console.WriteLine("Prime Series");
+            //bool flag = false;
+            for (int i = n; i <= m; i++)
+            {
+                if (IsPrime(i)) sum += i;
+            }
+            Console.WriteLine("Sum is: " + sum);
+        }
+        static bool IsPrime(int number)
+        {
+            bool flag = false;
+            for (int j = 2; j < number; j++)
+            {
+                if (number % j == 0) flag = true;
+            }
+            if (!flag)
+            {
+                Console.Write(number + ", ");
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_Programs
+{
+    class Program1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program to find Odd or Even Number!");
+            int number1 = 0;
+            if (number1 != 0)
+            {
+                if (number1 % 2 == 0)
+                {
+                    Console.WriteLine(number1 + " is Even");
+                }
+                else Console.WriteLine(number1 + " is Odd");
+            }       
+        }
+    }
+}
