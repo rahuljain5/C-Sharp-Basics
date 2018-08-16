52f332a7d8f5dc2ebc80ecc898f83bf5e56c787a
Rahul Jain <rjain.rahul5@gmail.com>
2018-08-16 11:01:25 +0530

    Added N-Queens Problem

---
+++ 8 Queens/EightQueensApplication/Backup/EightQueensGame/ChessBoard.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EightQueensGame
+{
+	public partial class ChessBoard : Form
+	{
+		Label[] labels = new Label[64];
+		GameStrategy game = new GameStrategy();
+		int numOfQueens = 0;
+
+		public ChessBoard()
+		{
+			InitializeComponent();
+			ChessBoardInit();
+		}
+
+		private void ChessBoardInit()
+		{
+			// Create the board
+			int id;
+			bool isCellWhite = true;
+			Color color = Color.White;
+			for (int i = 0; i < 64; ++i)
+			{
+				labels[i] = new Label();
+				labels[i].AutoSize = true;
+				if (isCellWhite)
+				{
+					color = Color.White;
+				}
+				else
+				{
+					color = Color.Black;
+				}
+				isCellWhite = !isCellWhite;
+				labels[i].BackColor = color;
+				labels[i].Dock = System.Windows.Forms.DockStyle.Fill;
+				labels[i].Location = new System.Drawing.Point(3, 0);
+				labels[i].Name = "label" + Convert.ToString(i);
+				labels[i].Size = new System.Drawing.Size(69, 58);
+				labels[i].TabIndex = 0;
+				labels[i].Text = "";
+				labels[i].Margin = new System.Windows.Forms.Padding(0);
+				tableLayoutPanel1.Controls.Add(labels[i]);
+				labels[i].Click += new EventHandler(Cell_Click);
+				id = i + 1;
+				labels[i].Tag = (object)id;
+				if ((i+1)%8 == 0)
+					isCellWhite = !isCellWhite;
+			}
+		}
+
+		void Cell_Click(object sender, EventArgs e)
+		{
+			Label cell = (Label)sender;
+			if (cell.Image != null)
+			{
+				cell.Image = null;
+				if (numOfQueens-- < 0)
+					numOfQueens = 0;
+				game.UnplaceQueen((int)cell.Tag);
+				return;
+			}
+
+			try 
+			{
+				if(game.isValidPostion((int)cell.Tag))
+				{
+					InsertQueen(cell);
+					if(numOfQueens==8)
+					{
+						MessageBox.Show("Success! You are the winner!");
+					}
+				}
+				else
+				{
+					Color temp = cell.BackColor;
+					for (int i = 0; i < 3; ++i)
+					{
+						cell.BackColor = Color.Red;
+						cell.Refresh();
+						System.Threading.Thread.Sleep(50);
+						cell.BackColor = Color.Yellow;
+						cell.Refresh();
+						System.Threading.Thread.Sleep(50);
+					}
+					cell.BackColor = temp;
+				}
+		
+			}
+			catch (GameOverException) 
+			{
+				MessageBox.Show("WINNER!!!!!!!!!!!!");
+			}			
+		}
+
+		private void InsertQueen(Label cell)
+		{
+			Image img = null;
+			if (cell.BackColor == Color.White)
+				img = Image.FromFile("D:\\TRAINING\\PRATIAN\\CONTENT\\DOTNET\\PROJECTS\\EightQueensGame\\VS2008\\EightQueensApplication\\EightQueensGame\\queens-crown-white.jpg");
+			else
+                img = Image.FromFile("D:\\TRAINING\\PRATIAN\\CONTENT\\DOTNET\\PROJECTS\\EightQueensGame\\VS2008\\EightQueensApplication\\EightQueensGame\\queens-crown-black.jpg");
+			cell.Image = img;
+			numOfQueens++;
+		}
+	}
+}
---
+++ 8 Queens/EightQueensApplication/Backup/EightQueensGame/GameStrategy.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EightQueensGame
+{
+	class GameStrategy
+	{
+        /// <summary>
+        /// Counter that keeps track of the number of Queens
+        /// placed on the chess board
+        /// </summary>
+		private int numQueens = 0;
+
+        /// <summary>
+        /// The 8x8 2-D array that corresponds to the positions
+        /// on the chess board
+        /// </summary>
+		private bool[,] placedQueens = new bool[8, 8];
+        
+        /// <summary>
+        /// Method to compute and return the Column number
+        /// as 0 through 7, given CellId
+        /// </summary>
+        /// <param name="cellId"></param>
+        /// <returns></returns>
+		private int getColumn(int cellId)
+		{
+			
+			/*
+                Logic to be written ....
+             */
+            return 0; // Just for the heck of it
+		}
+
+		
+        /// <summary>
+        /// Method to compute and return the Row number
+        /// as 0 through 7, given CellId
+        /// </summary>
+        /// <param name="cellId"></param>
+        /// <returns></returns>
+        private int getRow(int cellId)
+		{
+			/*
+                Logic to be written ....
+             */
+			return 0;// Just for the heck of it
+		}
+
+        /// <summary>
+        /// Method to check if the Cell is a valid position 
+        /// for placing the Queen
+        /// </summary>
+        /// <param name="cellId"></param>
+        /// <returns></returns>
+		public bool isValidPostion(int cellId)
+		{
+			bool isValid = true;
+
+			if (numQueens == 8) throw new GameOverException();
+
+			/*
+             * 
+             * Logic to be written to validate if there are
+             * any Queens already present in the path of
+             * the current cell
+             * 
+             * */
+
+			return isValid;
+		}
+
+       
+        /// <summary>
+        /// Method to remove a Queen from the cell
+        /// </summary>
+        /// <param name="cellId"></param>
+        public void UnplaceQueen(int cellId)
+        {
+            /*
+             
+             Logic to be written
+             */
+
+        }
+
+        // Other helper methods, need not be touched
+zz
+		public bool check(int row, int col, int index, int condn, bool rows, bool isValid)
+		{
+			for (int j = index; j < condn && isValid; j++)
+			{
+				if (rows)
+					isValid = !placedQueens[j, col];
+				else
+					isValid = !placedQueens[row, j];
+			}
+			return isValid;
+		}
+
+		public static void gameTest()
+		{
+			GameStrategy game = new GameStrategy();
+			int[] cellId = { 5, 11, 17, 31, 34, 48, 54, 60, 52 };
+			for (int i = 0; i < cellId.Length; i++)
+			{
+				try
+				{
+					MessageBox.Show(game.isValidPostion(5).ToString());
+				}
+				catch (GameOverException e)
+				{
+					MessageBox.Show("Game Over!!");
+				}
+			}
+		}
+	}
+}
+
